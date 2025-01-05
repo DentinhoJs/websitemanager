28f748939b4a0e1e51298e20eb9086a45f1f3c77
Dentinho Js <136614391+DentinhoJs@users.noreply.github.com>
2025-01-04 23:54:58 -0300

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-const path = require('path'); // Importa o módulo path para trabalhar com caminhos
-
-module.exports = (req, res) => {
-  const filePath = path.join(__dirname, '../frontend', 'index.html');
-  res.sendFile(filePath);
-};
